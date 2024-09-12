d083416b0172949dd131e427e241dfa89c054609
MINAH <tjdns7831@naver.com>
2024-09-12 23:27:30 +0900

    test(member): Service의 updateMember 메서드에 대한 테스트 코드 작성

--- src/test/java/com/dangun/miniproject/service/MemberServiceTest.java
+++ src/test/java/com/dangun/miniproject/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.dangun.miniproject.service;
 
 import com.dangun.miniproject.domain.Address;
 import com.dangun.miniproject.domain.Member;
+import com.dangun.miniproject.dto.GetAddressRequest;
 import com.dangun.miniproject.dto.GetMemberRequest;
 import com.dangun.miniproject.repository.AddressRepository;
 import com.dangun.miniproject.repository.MemberRepository;
@@ -12,12 +13,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
@@ -51,6 +55,7 @@ public class MemberServiceTest {
     }
 
 
+    // 회원 조회 테스트
     @Test
     public void getMember() {
         // Given
@@ -74,4 +79,34 @@ public class MemberServiceTest {
         assertEquals(address.getZipcode(), result.getAddress().getZipcode());
     }
 
+    // 회원 수정 테스트
+    @Test
+    void updateMember() {
+        // given
+        Long memberId = 1L;
+        GetMemberRequest updateRequest = new GetMemberRequest(
+                memberId,
+                "Hong@test.com",
+                "1234",
+                "Hong",
+                null // 주소는 업데이트하지 않으므로 null
+        );
+
+        when(memberRepository.findById(memberId)).thenReturn(Optional.of(member));
+        when(memberRepository.save(any(Member.class))).thenReturn(member);
+
+        // when
+        ResponseEntity<GetMemberRequest> response = memberService.updateMember(updateRequest, memberId);
+
+        // then
+        Optional<GetMemberRequest> optionalBody = Optional.ofNullable(response.getBody());
+        optionalBody.ifPresent(body -> {
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertEquals("Hong@test.com", body.getEmail());
+            assertEquals("1234", response.getBody().getPassword());
+            assertEquals("Hong", response.getBody().getNickname());
+            // 주소에 대한 검증은 생략
+        });
+
+    }
 }
