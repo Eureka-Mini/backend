b196c328eecbb9dc3b9f6b5fd210402ef83fbb19
kim-youngchul <kmosh1212@naver.com>
2024-09-23 00:32:17 +0900

    refactor: cors 제거

--- src/main/java/com/dangun/miniproject/common/config/SecurityConfig.java
+++ src/main/java/com/dangun/miniproject/common/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.dangun.miniproject.auth.filter.LoginFilter;
 import com.dangun.miniproject.auth.jwt.JWTUtil;
 import com.dangun.miniproject.auth.service.impl.TokenBlackListService;
 import com.dangun.miniproject.member.repository.MemberRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,10 +20,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -62,25 +57,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterAfter(jwtFilter, LoginFilter.class)
-                .addFilterBefore(jwtExceptionHandlerFilter, JWTFilter.class)
-
-                .cors((cors) -> cors
-                        .configurationSource(new CorsConfigurationSource() {
-
-                            @Override
-                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                                CorsConfiguration configuration = new CorsConfiguration();
-
-                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                                configuration.setAllowedMethods(Collections.singletonList("*"));
-                                configuration.setAllowedHeaders(Collections.singletonList("*"));
-                                configuration.setAllowCredentials(true);
-                                configuration.setMaxAge(3600L);
-                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-
-                                return configuration;
-                            }
-                        }));
+                .addFilterBefore(jwtExceptionHandlerFilter, JWTFilter.class);
 
         return http.build();
     }
